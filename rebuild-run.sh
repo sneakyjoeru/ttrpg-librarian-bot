ffe5feb5727138fa9704896b5be6bdd53269338e
Mihail Turusov <sneaky4oe@gmail.com>
2026-06-09 01:50:16 +0300

    chore: bot rename in rebuild

--- rebuild-run.sh
+++ rebuild-run.sh
@@ -1,2 +1,2 @@
-docker stop campaign-bot || true && docker rm campaign-bot || true
-docker build -t discord-campaign-bot . && docker run -d --name campaign-bot --restart unless-stopped discord-campaign-bot
+docker stop librarian-bot || true && docker rm librarian-bot || true
+docker build -t discord-librarian-bot . && docker run -d --name librarian-bot --restart unless-stopped discord-librarian-bot
