827cd146d99bd6fd765016f5d74aa3944a1925da
Mihail Turusov <sneaky4oe@gmail.com>
2026-06-11 02:50:23 +0300

    rebuild before container rerun.

--- rebuild-run.sh
+++ rebuild-run.sh
@@ -1,2 +1,4 @@
-docker stop librarian-bot || true && docker rm librarian-bot || true
-docker build -t discord-librarian-bot . && docker run -d --name librarian-bot --restart unless-stopped discord-librarian-bot
+docker build -t discord-librarian-bot . && \
+docker stop librarian-bot || true && \
+docker rm librarian-bot || true && \
+docker run -d --name librarian-bot --restart unless-stopped discord-librarian-bot
