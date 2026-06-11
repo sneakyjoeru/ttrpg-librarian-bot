dde7aef1dab0bfca450b7d365eada1df59d8dac3
Mihail Turusov <sneaky4oe@gmail.com>
2026-06-11 03:00:55 +0300

    recover secrets from docker ignore >_<

--- rebuild-run.sh
+++ rebuild-run.sh
@@ -1,4 +1,6 @@
 docker build -t discord-librarian-bot . && \
 docker stop librarian-bot || true && \
 docker rm librarian-bot || true && \
-docker run -d --name librarian-bot --restart unless-stopped discord-librarian-bot
+docker run -d --name librarian-bot --restart unless-stopped discord-librarian-bot && \
+sleep 15 && \
+docker logs librarian-bot
